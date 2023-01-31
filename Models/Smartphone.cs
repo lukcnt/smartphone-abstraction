a28e78c59a5fa381bcf05e15f0a595e5fa3bef33
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-30 21:26:10 -0300

    Create the methods for the class

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -5,11 +5,31 @@ using System.Threading.Tasks;
 
 namespace smartphone_abstraction.Models
 {
-    public class Smartphone
+    public abstract class Smartphone
     {
         public string Number { get; set; }
         private string Model { get; set; }
         private string Imei { get; set; }
-        private string Memory { get; set; }
+        private int Memory { get; set; }
+
+        public Smartphone(string number, string model, string imei, int memory)
+        {
+            Number = number;
+            Model = model;
+            Imei = imei;
+            Memory = memory;
+        }
+
+        public void Call()
+        {
+            Console.WriteLine("Calling...");
+        }
+
+        public void ReceiveCall()
+        {
+            Console.WriteLine("Receiving call...");
+        }
+
+        public abstract void InstallApp(string appName);
     }
 }
