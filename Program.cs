3e9de7190dace578a0ba091c47e253a531eb2ac8
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-30 21:52:47 -0300

    Create the tests for the classes and methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using smartphone_abstraction.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(number: "44998147466", model: "C20", imei: "1234562355", memory: 128);
+nokia.Call();
+nokia.InstallApp("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(number: "44999661782", model: "XR", imei: "7894564444", memory: 256);
+iphone.ReceiveCall();
+iphone.InstallApp("Instagram");
