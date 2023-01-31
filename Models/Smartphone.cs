ba83a7878bfe7cf4fffa6d7692514314241d6550
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-30 21:17:05 -0300

    Update the properties of the class

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -7,6 +7,9 @@ namespace smartphone_abstraction.Models
 {
     public class Smartphone
     {
-        
+        public string Number { get; set; }
+        private string Model { get; set; }
+        private string Imei { get; set; }
+        private string Memory { get; set; }
     }
 }
